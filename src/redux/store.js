1ae2e23fe19b09f1c25d7b51b7faad8cb2e2c75b
Ohleh <oleg.kolischuk@gmail.com>
2022-08-25 08:14:45 +0300

    f

--- src/redux/store.js
+++ src/redux/store.js
@@ -19,44 +19,6 @@ export const store = configureStore({
     [useGetContactsQuery.reducerPath]: useGetContactsQuery.reducer,
   },
 
-  //   reducer: {
-  //     [useGetContactsQuery.reducerPath]: useGetContactsQuery.reducer,
-  //   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(useGetContactsQuery.middleware),
 });
-
-// setupListeners(store.dispatch);
-
-// import { configureStore } from '@reduxjs/toolkit';
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-
-// import { phoneBookSlice } from './phoneBookSlice';
-
-// const persistConfig = {
-//   key: 'phoneBookPersist',
-//   version: 1,
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, phoneBookSlice.reducer);
-
-// export const store = configureStore({
-//   reducer: { phonebook: persistedReducer },
-//   middleware: getDefaultMiddleware =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
-
-// export const persistor = persistStore(store);
