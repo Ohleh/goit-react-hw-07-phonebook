315eae3976c08e7f5702d14c1f682bcf80552f96
Ohleh <oleg.kolischuk@gmail.com>
2022-08-25 08:08:17 +0300

    f

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,26 @@ import { configureStore } from '@reduxjs/toolkit';
 // Or from '@reduxjs/toolkit/query/react'
 // import { setupListeners } from '@reduxjs/toolkit/query';
 import { useGetContactsQuery } from './phoneApi';
+import { phoneBookSlice } from 'redux/phoneBookSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     [useGetContactsQuery.reducerPath]: useGetContactsQuery.reducer,
+//   },
+
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware().concat(useGetContactsQuery.middleware),
+// });
 
 export const store = configureStore({
   reducer: {
+    phonebook: phoneBookSlice.reducer,
     [useGetContactsQuery.reducerPath]: useGetContactsQuery.reducer,
   },
 
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(useGetContactsQuery.middleware),
+  //   reducer: {
+  //     [useGetContactsQuery.reducerPath]: useGetContactsQuery.reducer,
+  //   },
 });
 
 // setupListeners(store.dispatch);
